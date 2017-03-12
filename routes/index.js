ecfde0d39aa4d0f82e1c01259e9b102f23cf6d99
Kekura Musa <kekura.musa@gmail.com>
2017-03-12 19:35:55 +0000

    receive message data from facebook

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var mongo = require('mongodb');
 var assert = require('assert');
 var intents = require('../intents');
 
+const token = process.env.FB_VERIFY_TOKEN;
+const access = process.env.FB_ACCESS_TOKEN;
+
 //point to database
 var url = 'mongodb://localhost:27017/test';
 
@@ -28,21 +31,48 @@ router.post('/data', function(req, res, next) {
 });
 
 // Messenger Get and Post clients webhooks
-var FBtoken ="EAAaruV15FDsBADl9dLiBflf6eZCmretp1a4DBR8P7lkgWiup0ypI11YiM22iDKopMYTrYX2JOzABIATJJrJj4ukIq04PljjTCqLkZA2b5gtKVWOvRkJt1DerjTbv1aysuS7AJ6UbYDrlIBJZAxLEiCoFozSXZCqEKoZADgoMCFgZDZD"
 
 router.get('/messenger', function(req, res, next) {
 
-	if(req.query['hub.verify_token'] === 'solve_financial_intelligence') {
+	if(req.query['hub.verify_token'] === token) {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
 })
 
-router.post('/messenger', function(req, res, next) {
+app.post('/messenger', function (req, res) {
+  var data = req.body;
 
-	console.log('post to messenger')
-})
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
 
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
 
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  console.log("Message data: ", event.message);
+}
 
 module.exports = router;
