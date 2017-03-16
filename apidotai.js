5478fa32c7d809a658044bb53a71f67a658110e6
Kekura Musa <kekura.musa@gmail.com>
2017-03-16 23:52:44 +0000

    fixed broken apiai referrence

--- apidotai.js
+++ apidotai.js
@@ -1,18 +1,21 @@
-var app = apiai("2fe8e7eb2ffc4d428e85f13eb8066e6c");
+module.exports = {
 
-function classifyMessage(message, sessionId) {
+	classifyMessage: function(message, sessionId) {
 
-	var request = app.textRequest(message, {
-	    sessionId: sessionId
-	});
+		var app = apiai("2fe8e7eb2ffc4d428e85f13eb8066e6c");
 
-	request.on('response', function(response) {
-	    console.log(response);
-	});
+		var request = app.textRequest(message, {
+		    sessionId: sessionId
+		});
 
-	request.on('error', function(error) {
-	    console.log(error);
-	});
+		request.on('response', function(response) {
+		    console.log(response);
+		});
 
-	request.end();
+		request.on('error', function(error) {
+		    console.log(error);
+		});
+
+		request.end();
+	}
 }
